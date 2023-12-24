2b4db11a74835320b4dbdbad6dd561dc073b0421
Joe Drago <joedrago@gmail.com>
2023-12-24 11:51:59 -0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const { main } = require("./skittles")
+
+main(process.argv.slice(2))
---
+++ src/skittles.js
@@ -0,0 +1,12 @@
+class Skittles {
+    constructor() {}
+}
+
+const main = (argv) => {
+    console.log("skittles main")
+}
+
+module.exports = {
+    Skittles,
+    main
+}
