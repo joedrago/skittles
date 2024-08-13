bf7e67e7202f25cd6166590888dbafa3fed20a77
Joe Drago <joedrago@gmail.com>
2024-08-13 13:50:19 -0700

    Add skipEncode option to video mod

--- mods/video.js
+++ mods/video.js
@@ -41,10 +41,18 @@ const request = async (req, key, capture) => {
         return req.reply({ text: `Failed to download video: ${url}` })
     }
 
-    console.log(`Reencoding ${downloadFilename} => ${downloadFilename}`)
-    const reencoded = await skittles.spawn("ffmpeg", ["-i", downloadFilename, reencodeFilename, "-c:v", "h264", "-c:a", "aac"])
-    if (!reencoded || !fs.existsSync(reencodeFilename)) {
-        return req.reply({ text: `Failed to reencode video: ${url}` })
+    if(req.action.skipEncode) {
+        console.log(`Copying ${downloadFilename} => ${reencodeFilename}`)
+        const copied = await skittles.spawn("cp", [downloadFilename, reencodeFilename])
+        if (!copied || !fs.existsSync(reencodeFilename)) {
+            return req.reply({ text: `Failed to copy video: ${url}` })
+        }
+    } else {
+        console.log(`Reencoding ${downloadFilename} => ${reencodeFilename}`)
+        const reencoded = await skittles.spawn("ffmpeg", ["-i", downloadFilename, reencodeFilename, "-c:v", "h264", "-c:a", "aac"])
+        if (!reencoded || !fs.existsSync(reencodeFilename)) {
+            return req.reply({ text: `Failed to reencode video: ${url}` })
+        }
     }
 
     req.reply({ text: `=>`, files: [reencodeFilename] })
